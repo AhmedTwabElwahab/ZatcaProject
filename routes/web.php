105110f9b35db937bf967b906d91d0156d280135
Ahmed Twab El Wahab <128147750+AhmedTwabElwahab@users.noreply.github.com>
2025-06-28 13:31:06 +0300

    update zatca info

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\ZatcaIntegrationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/zatca', [ZatcaIntegrationController::class, 'index'])->name('zatca');
